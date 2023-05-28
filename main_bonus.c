972f2fa443b6f0bfd9cde4335fa3eb71b79353b2
Bilal El Idrissi <bilalelidrissi171@gmail.com>
2023-05-28 13:03:00 +0100

    Add main_bonus.c

---
+++ main_bonus.c
@@ -0,0 +1,17 @@
+#include "minishell_bonus.h"
+
+int main(int argc, char *argv[], char *env[])
+{
+	(void)argc;
+	(void)argv;
+	(void)env;
+	// char *line;
+	// t_data data;
+
+	// while ((line = readline("minishell$ ")))
+	// {
+
+	// 	free(line);
+	// }
+	return (0);
+}
