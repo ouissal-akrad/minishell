0780af1bfe8c4f7b7fc85a4bac430688841339f3
Bilal El Idrissi <bilalelidrissi171@gmail.com>
2023-05-28 13:02:41 +0100

    Add main.c

--- main.c
+++ main.c
@@ -1,1 +1,23 @@
-#include <stdio.h>
+#include "minishell.h"
+
+int main(int argc, char *argv[], char *env[])
+{
+	(void)argc;
+	(void)argv;
+	(void)env;
+
+	char *line;
+	// t_data data;
+
+	while ((line = readline("minishell$ ")))
+	{
+		// data = ft_parsing(argv, env);
+
+
+
+
+
+		free(line);
+	}
+	return (0);
+}
