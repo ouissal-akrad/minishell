20410bbc27062799e594d039dddff2e2355e6a2e
ouissal-akrad <=>
2023-07-15 17:21:25 +0100

    export

--- mandatory/executing/builtins/my_export.c
+++ mandatory/executing/builtins/my_export.c
@@ -6,7 +6,7 @@
 /*   By: ouakrad <ouakrad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 16:46:00 by ouakrad           #+#    #+#             */
-/*   Updated: 2023/07/15 15:49:22 by ouakrad          ###   ########.fr       */
+/*   Updated: 2023/07/15 17:17:08 by ouakrad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,7 @@ t_env	*ft_csp(t_env *env, char *s, int c)
 	char	*prev;
 	char	*rest;
 	char	*tmp;
-
+	char	*to_find;
 	i = 0;
 	prev = NULL;
 	rest = NULL;
@@ -97,11 +97,22 @@ t_env	*ft_csp(t_env *env, char *s, int c)
 		return (env);
 	if (tmp[0] != '=')
 	{
-		if (ft_strchr(tmp,'=') == NULL && tmp)
-			ft_lstadd_backk(&env, ft_lstneww(s, NULL));
+
+		//no args
+		if (ft_strchr(tmp,'=') == NULL)
+		{
+			if(ft_isalpha(tmp[0]))
+				ft_lstadd_backk(&env, ft_lstneww(s, NULL));
+			else 
+			{
+				printf("minishell: export: %s: not a valid identifier\n", s);
+				free(tmp);
+				return (env);
+			}
+		}
+		//with args
 		else if(ft_strchr(tmp,'=') != NULL)
 		{
-			
 			while (tmp[i])
 			{
 				if (tmp[i] == c)
@@ -141,12 +152,18 @@ t_env	*ft_csp(t_env *env, char *s, int c)
 					free(tmp);
 					return (env);
 				}
-				puts("sec");
 				ft_lstadd_backk(&env, ft_lstneww(prev, rest));
 			}
 		}
 		free(tmp);
+		printf("----\n");
 		print_env_ex(env);
+		printf("----\n");
 		}
 	return (env);
 }
+
+// chi variable deja kayna, ila drtiliha export kat ecrasa value dialha w kadar blastha
+// multiple variables f commande whda
+// export a="adnane" a+="harib" , concatenehum hh
+// 
